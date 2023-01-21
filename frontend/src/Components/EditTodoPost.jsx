d665bb86567ddb360ca9cde85b07ccb41db19e55
rohit yadav <yadavry09@gmail.com>
2023-01-21 21:46:47 +0530

    fixed udate modal

--- frontend/src/Components/EditTodoPost.jsx
+++ frontend/src/Components/EditTodoPost.jsx
@@ -10,11 +10,30 @@ import {
     Button,
     Input,
     Textarea,
+    Select,
+    FormControl,
+    FormLabel,
   } from '@chakra-ui/react'
+import { useState } from 'react'
 
 
-export function UpdateTodoPost() {
+export function UpdateTodoPost({title, description, todoStatus}) {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const  [editTodo , setEditTodo] = useState({
+          title: title,
+          description : description,
+          todoStatus: todoStatus
+    })
+
+    const handleChange = (e)=>{
+         const {name, value} = e.target;
+
+          setEditTodo({
+            ...editTodo,
+            [name] : value
+          })
+    }
+
     return (
       <>
         <Button colorScheme={"green"} onClick={onOpen}>Update</Button>
@@ -25,8 +44,22 @@ export function UpdateTodoPost() {
             <ModalHeader>Update</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <Input />
-              <Textarea />
+                <form action="">
+
+                <FormControl mb="15px">
+                    <FormLabel>Title</FormLabel>
+                    <Input onChange={handleChange} name="title" value={editTodo.title} placeholder='title' />
+                </FormControl>
+                <FormLabel>Status</FormLabel>
+                <Select onChange={handleChange} name='todoStatus' value={editTodo.todoStatus} mb="10px"  placeholder='Status'>
+                     <option value='pending'>Pending</option>
+                     <option value='inprogress'>In Progress</option>
+                     <option value='done'>Done</option>
+                </Select> 
+                
+
+              <Textarea onChange={handleChange} resize={"none"} name="description" value={editTodo.description} placeholder='description'/>
+                </form>
             </ModalBody>
   
             <ModalFooter>
--- frontend/src/Routes/Home.jsx
+++ frontend/src/Routes/Home.jsx
@@ -92,33 +92,10 @@ const handleSubmit=(e)=>{
                                  boxShadow="0px 10px 15px -3px rgba(0,0,0,0.1)"
                                  margin="0.5rem auto 1rem"
                                  padding="15px"
+                                 key={el._id}
                                 >
                                   <TodoCard key={el._id} {...el}/>
-                                   {/* <Text 
-                                     mb="5px"
-                                     fontSize={"19px"} fontWeight={"600"}
-                                   >
-                                     Title :   <Text as="span">{el.title}</Text>
-                                    </Text> 
-                                     <Badge>
-                                      {el.todoStatus}
-                                     </Badge>
-                                      <Text mb="7px" fontSize={"19px"} fontWeight={"600"}>Description </Text>
-                                     <Text>
-                                         {
-                                          el.description
-                                         }
-                                      </Text> 
-
-                                      <Flex mt="15px">
-                                         <Button colorScheme={"green"}>
-                                                     Update
-                                         </Button>
-                                         <Spacer />
-                                         <Button colorScheme={"red"}>
-                                                       Delete
-                                         </Button>
-                                      </Flex>*/}
+                                   
                                 </Box> 
                            })
                           
